78e68925862bc441b20c87b984734c3d66907bf6
elifnurtomaz <tmz.elifnur@gmail.com>
2026-07-30 22:55:11 +0300

    Add port range scanning

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from scanner import scan_port
 
 ip = input("Enter IP address: ")
 
-port = int(input("Enter port: "))
+start_port = int(input("Start port: "))
+end_port = int(input("End port: "))
 
-if scan_port(ip, port):
-    print(f"Port {port} is OPEN.")
-else:
-    print(f"Port {port} is CLOSED.")
+print("\nScanning...\n")
+
+open_ports = []
+
+for port in range(start_port, end_port + 1):
+
+    if scan_port(ip, port):
+        print(f"Port {port} is OPEN.")
+        open_ports.append(port)
+
+print("\nScan completed.")
+
+print(f"\nOpen ports found: {len(open_ports)}")
