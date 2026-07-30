2841a07e55530552ba886216c06e5ae1a958d623
elifnurtomaz <tmz.elifnur@gmail.com>
2026-07-30 23:31:38 +0300

    Add command-line argument support

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+import argparse
+
 from report import save_report
 import time
 from scanner import scan_port
 
-ip = input("Enter IP address: ")
+parser = argparse.ArgumentParser(
+    description="Simple TCP Port Scanner"
+)
 
-start_port = int(input("Start port: "))
-end_port = int(input("End port: "))
+parser.add_argument(
+    "--host",
+    required=True,
+    help="Target IP address or hostname"
+)
+
+parser.add_argument(
+    "--start",
+    type=int,
+    required=True,
+    help="Start port"
+)
+
+parser.add_argument(
+    "--end",
+    type=int,
+    required=True,
+    help="End port"
+)
+
+args = parser.parse_args()
+
+ip = args.host
+start_port = args.start
+end_port = args.end
 
 print("\nScanning...\n")
 
